8c78c516a6c4a8830a505b0958c46a8a26ce3662
sangita awaghad <sangita.awaghad@gmail.com>
2020-01-02 17:33:15 +0530

    ADDED:code & test given null when unparked should throw error

--- main/ParkingLots.js
+++ main/ParkingLots.js
@@ -15,6 +15,9 @@ class ParkingLots {
         if(vehicle===undefined)
             throw new Error('vehicle can not be undefined');
 
+        if(vehicle==null)
+            throw new Error('vehicle can not be null');
+
         if (this.vehicle ===vehicle) {
             this.vehicle = null;
             return true;
