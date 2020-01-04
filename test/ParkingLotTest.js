bd6a1aeed7155a222443e3d24e2a9d87b49552f6
sangita awaghad <sangita.awaghad@gmail.com>
2020-01-04 10:04:37 +0530

    REFACTORED:separate describe block for find vehicle and finding empty slot in parking lot

--- test/ParkingLotTest.js
+++ test/ParkingLotTest.js
@@ -1,8 +1,8 @@
-let assert=require('assert');
-let Parkinglot=require('../main/ParkingLots');
-let vehicle=require('../main/Vehicle');
+let assert = require('assert');
+let Parkinglot = require('../main/ParkingLots');
+let vehicle = require('../main/Vehicle');
 
-describe('test for parking vehicle in parking lot',()=> {
+describe('test for parking vehicle in parking lot', () => {
     it('given vehicle when parked should return false', () => {
         let vehicleObj = new vehicle.Vehicle();
         assert.equal(new Parkinglot.ParkingLots(1).park(vehicleObj), false);
@@ -26,7 +26,7 @@ describe('test for parking vehicle in parking lot',()=> {
 
     it('given vehicle when parked and lot is full should return true', () => {
         let vehicle1 = new vehicle.Vehicle();
-        let parkingLot=new Parkinglot.ParkingLots(1)
+        let parkingLot = new Parkinglot.ParkingLots(1)
         parkingLot.park(vehicle1);
         assert.equal(parkingLot.isFull(), true);
     });
@@ -34,28 +34,28 @@ describe('test for parking vehicle in parking lot',()=> {
     it('when parking lot is full informed to airport security should return lot is Full', () => {
         let vehicle1 = new vehicle.Vehicle();
         Parkinglot.parkinglotObject.park(vehicle1);
-        assert.equal( Parkinglot.parkinglotObject.park(new vehicle.Vehicle()), "lot is Full");
+        assert.equal(Parkinglot.parkinglotObject.park(new vehicle.Vehicle()), "lot is Full");
     });
 
     it('when parking lot is full informed to parking lot owner should return lot is Full', () => {
         let vehicle1 = new vehicle.Vehicle();
         Parkinglot.parkinglotObject.park(vehicle1);
-        assert.equal( Parkinglot.parkinglotObject.park(new vehicle.Vehicle()), "lot is Full");
+        assert.equal(Parkinglot.parkinglotObject.park(new vehicle.Vehicle()), "lot is Full");
     });
 });
 
-describe('test for un parking vehicle from parking lot',()=> {
-    it('given vehicle parked and when un parked should return true',()=>{
-        let vehicleObj=new vehicle.Vehicle(1);
-        let parkingLotObject=new Parkinglot.ParkingLots();
+describe('test for un parking vehicle from parking lot', () => {
+    it('given vehicle parked and when un parked should return true', () => {
+        let vehicleObj = new vehicle.Vehicle(1);
+        let parkingLotObject = new Parkinglot.ParkingLots();
         parkingLotObject.park(vehicleObj);
-        assert.equal(parkingLotObject.unPark(vehicleObj),true);
+        assert.equal(parkingLotObject.unPark(vehicleObj), true);
     });
 
     it('given undefined when un parked should return throw error', () => {
         try {
-            let vehicleObj=new vehicle.Vehicle(1);
-            let parkingLotObject=new Parkinglot.ParkingLots();
+            let vehicleObj = new vehicle.Vehicle(1);
+            let parkingLotObject = new Parkinglot.ParkingLots();
             parkingLotObject.park(vehicleObj);
             parkingLotObject.unPark(undefined);
         } catch (e) {
@@ -65,8 +65,8 @@ describe('test for un parking vehicle from parking lot',()=> {
 
     it('given null when un parked should return throw error', () => {
         try {
-            let vehicleObj=new vehicle.Vehicle(1);
-            let parkingLotObject=new Parkinglot.ParkingLots();
+            let vehicleObj = new vehicle.Vehicle(1);
+            let parkingLotObject = new Parkinglot.ParkingLots();
             parkingLotObject.park(vehicleObj);
             parkingLotObject.unPark(undefined);
         } catch (e) {
@@ -77,11 +77,11 @@ describe('test for un parking vehicle from parking lot',()=> {
     it('when parking lot space is available informed to parking lot owner should return lot free space available', () => {
         let vehicle1 = new vehicle.Vehicle();
         Parkinglot.parkinglotObject.park(vehicle1);
-        assert.equal( Parkinglot.parkinglotObject.unPark(vehicle1), "free space available");
+        assert.equal(Parkinglot.parkinglotObject.unPark(vehicle1), "free space available");
     });
 });
 
-describe('test for parking lot is full',()=> {
+describe('test for parking lot is full', () => {
     it('when parking lot is full informed to parking lot owner should return lot is Full', () => {
         let vehicle1 = new vehicle.Vehicle();
         Parkinglot.parkinglotObject.park(vehicle1);
@@ -95,37 +95,40 @@ describe('test for parking lot is full',()=> {
         let vehicle2 = new vehicle.Vehicle();
         assert.equal(Parkinglot.parkinglotObject.park(vehicle2), "lot is Full");
     });
+});
 
+describe('test for checking empty parking slot', () => {
     it('given vehicles when parked and two unparked then one parked should informed available empty slot', () => {
         let vehicle1 = new vehicle.Vehicle();
-        let parkingLotObject=new Parkinglot.ParkingLots(5);
-        parkingLotObject.park(vehicle1,0);
+        let parkingLotObject = new Parkinglot.ParkingLots(5);
+        parkingLotObject.park(vehicle1, 0);
         let vehicle2 = new vehicle.Vehicle();
-        parkingLotObject.park(vehicle2,1);
+        parkingLotObject.park(vehicle2, 1);
         let vehicle3 = new vehicle.Vehicle();
-        parkingLotObject.park(vehicle3,2);
+        parkingLotObject.park(vehicle3, 2);
         let vehicle4 = new vehicle.Vehicle();
-        parkingLotObject.park(vehicle4,3);
+        parkingLotObject.park(vehicle4, 3);
         let vehicle5 = new vehicle.Vehicle();
-        parkingLotObject.park(vehicle5,4);
+        parkingLotObject.park(vehicle5, 4);
         parkingLotObject.unPark(vehicle2);
         parkingLotObject.unPark(vehicle5);
-        parkingLotObject.park(new vehicle.Vehicle(),1);
-        let arr=parkingLotObject.giveEmptySlots();
+        parkingLotObject.park(new vehicle.Vehicle(), 1);
+        let arr = parkingLotObject.giveEmptySlots();
         assert.equal(arr, 4);
     });
+});
 
+describe('test for finding vehicle in parking lot', () => {
     it('given vehicle should return slotnumber of parkinglot', () => {
-
         let vehicle1 = new vehicle.Vehicle();
-        let parkingLotObject=new Parkinglot.ParkingLots(3);
-        parkingLotObject.park(vehicle1,0);
+        let parkingLotObject = new Parkinglot.ParkingLots(3);
+        parkingLotObject.park(vehicle1, 0);
         let vehicle2 = new vehicle.Vehicle();
-        parkingLotObject.park(vehicle2,1);
+        parkingLotObject.park(vehicle2, 1);
         let vehicle3 = new vehicle.Vehicle();
-        parkingLotObject.park(vehicle3,2);
-        let result=parkingLotObject.findMyVehicle(vehicle3);
-        assert.equal(result,2);
+        parkingLotObject.park(vehicle3, 2);
+        let result = parkingLotObject.findMyVehicle(vehicle3);
+        assert.equal(result, 2);
 
-})
+    })
 });
